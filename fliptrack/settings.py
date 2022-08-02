9b5007d01c569b1382d06f9b7f088407f579c195
Safeer <safeersfm@gmail.com>
2022-08-02 16:45:32 +0530

    heroku 1.2

--- fliptrack/settings.py
+++ fliptrack/settings.py
@@ -14,7 +14,9 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
