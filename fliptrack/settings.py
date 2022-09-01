ac39265b007022131f889f4d490a9809f8422cc3
Safeer <safeersfm@gmail.com>
2022-09-01 22:06:42 +0530

    Heroku 3.6.2

--- fliptrack/settings.py
+++ fliptrack/settings.py
@@ -30,7 +30,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'n')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['flicktracks.herokuapp.com','127.0.0.1']
 
