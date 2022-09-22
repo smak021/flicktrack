2022aa290bf8c56a082f61cd6612a2dc27c352eb
Safeer <safeersfm@gmail.com>
2022-09-23 00:25:20 +0530

    B

--- fliptrack/settings.py
+++ fliptrack/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'n')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = ['https://flicktracks.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['flicktracks.herokuapp.com','127.0.0.1']
 
 
 # Application definition
